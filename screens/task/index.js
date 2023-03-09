4f4cc9086efe04007fd9dd413a58d3ef8c77d66b
Spoofii-pdf <112996149+Spoofii-pdf@users.noreply.github.com>
2023-03-10 07:46:14 +0800

    added task containers to Task page as placeholders

--- screens/task/index.js
+++ screens/task/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { View, Pressable, Text, Image } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { SelectList } from 'react-native-dropdown-select-list'
+import { StyleSheet, View, Pressable, Text, Image, TouchableOpacity } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
 
 
 export function TaskScreen({ navigation }) {
@@ -27,6 +27,119 @@ export function TaskScreen({ navigation }) {
               save="value"
           />
     
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>Task 1</Text>
+                <TouchableOpacity onPress={() => props.checkTask()}>
+                    <AntDesign style={styles.check} name="eyeo" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.editTask()}>
+                    <AntDesign style={styles.edit} name="edit" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={22} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>Task 2</Text>
+                <TouchableOpacity onPress={() => props.checkTask()}>
+                    <AntDesign style={styles.check} name="eyeo" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.editTask()}>
+                    <AntDesign style={styles.edit} name="edit" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={22} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>Task 3</Text>
+                <TouchableOpacity onPress={() => props.checkTask()}>
+                    <AntDesign style={styles.check} name="eyeo" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.editTask()}>
+                    <AntDesign style={styles.edit} name="edit" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={22} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>Task 4</Text>
+                <TouchableOpacity onPress={() => props.checkTask()}>
+                    <AntDesign style={styles.check} name="eyeo" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.editTask()}>
+                    <AntDesign style={styles.edit} name="edit" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={22} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>Task 5</Text>
+                <TouchableOpacity onPress={() => props.checkTask()}>
+                    <AntDesign style={styles.check} name="eyeo" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.editTask()}>
+                    <AntDesign style={styles.edit} name="edit" size={22} color='#fff' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={22} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+
       </View>
+
+      
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+    },
+    index: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    taskContainer: {
+        backgroundColor: '#7B1113',
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        minHeight: 50,
+        marginTop: 20,
+    },
+    task: {
+        color: '#fff',
+        width: '90%',
+        fontSize: 16,
+    },
+    check: {
+      marginLeft: -50,
+    },
+    edit: {
+      marginLeft: -25,
+    },
+    delete: {
+        marginLeft: 0,
+    },
+});
