142d2f34625759583b9916117a0740c6038645e9
Spoofii-pdf <112996149+Spoofii-pdf@users.noreply.github.com>
2023-03-10 07:02:18 +0800

    Added drop down list for tasks list

--- screens/task/index.js
+++ screens/task/index.js
@@ -1,10 +1,32 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Pressable, Text, Image } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SelectList } from 'react-native-dropdown-select-list'
+
 
 export function TaskScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Task Screen</Text>
+  const [selected, setSelected] = React.useState("");
+
+  const data = [
+      {key:'1', value:'Today'},
+      {key:'2', value:'Pending'},
+      {key:'3', value:'Overdue'},
+      {key:'4', value:'Tomorrow'},
+      {key:'5', value:'This Week'},
+      {key:'6', value:'Completed'},
+  ]
+
+    return(
+      <View style = {{paddingHorizontal:20, paddingVertical:50, flex:1}}>
+    
+        <SelectList 
+              setSelected={(val) => setSelected(val)} 
+              search={false}
+              placeholder='Today'
+              data={data} 
+              save="value"
+          />
+    
       </View>
     );
   }
