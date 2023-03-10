8f0718fb092bd3bee67ebadd44319d84210e65ac
mrtapia <mrtapia@up.edu.ph>
2023-03-10 09:08:42 +0800

    added KeyboardAvoidingView, updated auth flow

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -10,60 +10,56 @@ import { HomeScreen } from './screens/home';
 import { ScheduleScreen } from './screens/schedule';
 import { TaskScreen } from './screens/task';
 import { DashboardScreen } from './screens/dashboard';
+import { AuthContext } from './contexts/AuthContext';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const isLoggedin = false;
-
 export default function App() {
-  return ( isLoggedin ? 
 
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Schedule" component={ScheduleScreen} />
-          <Tab.Screen name="Task" component={TaskScreen} />
-          <Tab.Screen name="Dashboard" component={DashboardScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-    :
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="StartUp"
-            component={StartUpScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUpScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="PoutchiSignUp"
-            component={PoutchiSignUpScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  return ( 
+    <>
+      <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+        <SafeAreaProvider>
+        { isLoggedIn ? (
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Schedule" component={ScheduleScreen} />
+              <Tab.Screen name="Task" component={TaskScreen} />
+              <Tab.Screen name="Dashboard" component={DashboardScreen} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        ):(
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="StartUp"
+                component={StartUpScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="SignUp"
+                component={SignUpScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="PoutchiSignUp"
+                component={PoutchiSignUpScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        )}
+        </SafeAreaProvider>
+      </AuthContext.Provider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/entry/login.js
+++ screens/entry/login.js
@@ -1,38 +1,50 @@
 import * as React from 'react';
-import { View, Pressable, Text, TextInput, Image, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { View, Pressable, Text, TextInput, Image, Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView, Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export function LoginScreen({ navigation }) {
   const insets = useSafeAreaInsets();
+  const {setIsLoggedIn} = React.useContext(AuthContext);
+
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  }
+
     return (
-        <TouchableWithoutFeedback  onPress={Keyboard.dismiss} accessible={false}>
-            <View className="bg-[#407BB3] flex-auto justify-center items-center" style={{
-                paddingTop: insets.top,
-                paddingBottom: insets.bottom,
-                paddingLeft: insets.left,
-                paddingRight: insets.right,
-            }}>
-                <View className="flex-row items-center mb-12">
-                    <Image source={require("../../assets/upoutchi-logo-dark.png")} />
-                    <Text className="text-[#232528] text-5xl font-extrabold mt-4 ml-1">UPoutchi</Text>
+        <View className="bg-[#407BB3] flex-auto" style={{
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+        }}>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} className="flex-auto">
+            <TouchableWithoutFeedback  onPress={Keyboard.dismiss} accessible={false}>
+                <View className="flex-auto justify-center items-center">
+                    <View className="flex-row items-center mb-12">
+                        <Image source={require("../../assets/upoutchi-logo-dark.png")} />
+                        <Text className="text-[#232528] text-5xl font-extrabold mt-4 ml-1">UPoutchi</Text>
+                    </View>
+                    <View className="mb-4">
+                        <Text className="text-white text-base mb-2">Email address</Text>
+                        <TextInput className="bg-white text-[16px] rounded p-2 w-64"></TextInput>
+                    </View>
+                    <View className="mb-4">
+                        <Text className="text-white text-base mb-2">Password</Text>
+                        <TextInput className="bg-white text-[16px] rounded p-2 w-64" secureTextEntry={true}></TextInput>
+                    </View>
+                    <Pressable className="my-8"
+                    onPress={loginHandler}
+                    >
+                    {({pressed}) => (
+                        <View className={`w-40 py-2 rounded-lg shadow shadow-gray-800 ${pressed ? "bg-[#C84C48]" : "bg-[#EE7A76]"}`}>
+                        <Text className="text-white text-lg text-center font-semibold">LOGIN</Text>
+                    </View>
+                    )}
+                    </Pressable>
                 </View>
-                <View className="mb-4">
-                    <Text className="text-white text-base mb-2">Email address</Text>
-                    <TextInput className="bg-white text-[16px] rounded p-2 w-64"></TextInput>
-                </View>
-                <View className="mb-4">
-                    <Text className="text-white text-base mb-2">Password</Text>
-                    <TextInput className="bg-white text-[16px] rounded p-2 w-64" secureTextEntry={true}></TextInput>
-                </View>
-                <Pressable className="my-8"
-                >
-                {({pressed}) => (
-                    <View className={`w-40 py-2 rounded-lg shadow shadow-gray-800 ${pressed ? "bg-[#C84C48]" : "bg-[#EE7A76]"}`}>
-                    <Text className="text-white text-lg text-center font-semibold">LOGIN</Text>
-                </View>
-                )}
-                </Pressable>
-            </View>
-      </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+      </View>
     );
   }
