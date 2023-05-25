872b1e5c0b2ee9783e82e1860960318f34c76245
Angelo Convento <convento.angelo@gmail.com>
2023-05-26 01:08:57 +0800

    Focus timer resets to 25min -avoid looping rewards

--- screens/focus/index.js
+++ screens/focus/index.js
@@ -1,15 +1,20 @@
 import * as React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import {
-	CountdownCircleTimer,
-} from "react-native-countdown-circle-timer";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 import { Ionicons } from "@expo/vector-icons";
 import { NewItemModal } from "../../components/shared/newItemModal";
 
 import { UserAuth } from "../../contexts/AuthContext";
 import { UserInfoContext } from "../../contexts/AuthContext";
 
-import { collection, addDoc, setDoc, doc, deleteDoc, Timestamp } from "firebase/firestore";
+import {
+	collection,
+	addDoc,
+	setDoc,
+	doc,
+	deleteDoc,
+	Timestamp,
+} from "firebase/firestore";
 import { db } from "../../lib/firebase";
 
 export function FocusScreen({ navigation }) {
@@ -24,10 +29,10 @@ export function FocusScreen({ navigation }) {
 	const stopHandler = async () => {
 		await addDoc(collection(db, "users", user.uid, "sessions"), {
 			time: time.current,
-			date: Timestamp.now()
+			date: Timestamp.now(),
 		});
-		setSessions([...sessions, {time: time.current, date: datenow}]);
-	}
+		setSessions([...sessions, { time: time.current, date: datenow }]);
+	};
 
 	const togglePause = () => {
 		if (isStart) setIsStart(false);
@@ -50,68 +55,74 @@ export function FocusScreen({ navigation }) {
 
 	return (
 		<>
-		<NewItemModal visible={newItemModalVisible} setVisible={setNewItemModalVisible} action={"SESSION"}/>
-		<View style={styles.container}>
-			<CountdownCircleTimer
-				key={key}
-				size={280}
-				strokeWidth={20}
-				duration={1500}
-				colors={["#439cfb", "#E5203E", "#FFFFFF"]}
-				colorsTime={[10, 0.0001, 0]}
-				isPlaying={!isPaused}
-				trailColor="#111010"
-				trailStrokeWidth={7}
-				rotation="counterclockwise"
-			>
-				{({ remainingTime, color }) => (
-					<Text style={{ fontSize: 50, fontWeight: "bold", color }}>
-						{children({ remainingTime })}
-					</Text>
-				)}
-			</CountdownCircleTimer>
-			<View style={{ flexDirection: "row", marginTop: 20 }}>
-				<View style={styles.btnContainer}>
-					<Ionicons
-						name={isPaused ? "md-play-circle" : "md-pause-circle"}
-						title={isPaused ? "Resume" : "Pause"}
-						onPress={togglePause}
-						size={80}
-						color={isPaused ? "#439cfb" : "#E5203E"}
-					/>
-				</View>
-				<View style={styles.btnContainer}>
-					<Ionicons 
-					name="stop" 
-					title="Stop" 
-					disabled={isStart}
-					onPress={() => {
-						setNewItemModalVisible(true);
-						setIsPaused(true);
-						setIsStart(true);
-						stopHandler();
-						setKey((prevKey) => prevKey + 1);
+			<NewItemModal
+				visible={newItemModalVisible}
+				setVisible={setNewItemModalVisible}
+				action={"SESSION"}
+			/>
+			<View style={styles.container}>
+				<CountdownCircleTimer
+					key={key}
+					size={280}
+					strokeWidth={20}
+					duration={1500}
+					colors={["#439cfb", "#E5203E", "#FFFFFF"]}
+					colorsTime={[1500, 0.0001, 0]}
+					isPlaying={!isPaused}
+					trailColor="#111010"
+					trailStrokeWidth={7}
+					rotation="counterclockwise"
+					onComplete={() => {
+						return { shouldRepeat: true, delay: 0.5 };
 					}}
-					size={80} 
-					color={isStart ? "#111010" : "#E5203E"} />
-					
+				>
+					{({ remainingTime, color }) => (
+						<Text style={{ fontSize: 50, fontWeight: "bold", color }}>
+							{children({ remainingTime })}
+						</Text>
+					)}
+				</CountdownCircleTimer>
+				<View style={{ flexDirection: "row", marginTop: 20 }}>
+					<View style={styles.btnContainer}>
+						<Ionicons
+							name={isPaused ? "md-play-circle" : "md-pause-circle"}
+							title={isPaused ? "Resume" : "Pause"}
+							onPress={togglePause}
+							size={80}
+							color={isPaused ? "#439cfb" : "#E5203E"}
+						/>
+					</View>
+					<View style={styles.btnContainer}>
+						<Ionicons
+							name="stop"
+							title="Stop"
+							disabled={isStart}
+							onPress={() => {
+								setNewItemModalVisible(true);
+								setIsPaused(true);
+								setIsStart(true);
+								stopHandler();
+								setKey((prevKey) => prevKey + 1);
+							}}
+							size={80}
+							color={isStart ? "#111010" : "#E5203E"}
+						/>
+					</View>
+					<View style={styles.btnContainer}>
+						<Ionicons
+							name="md-reload-circle"
+							title="Restart"
+							onPress={() => {
+								setKey((prevKey) => prevKey + 1);
+								setIsPaused(true);
+								setIsStart(true);
+							}}
+							size={80}
+							color={"#111010"}
+						/>
+					</View>
 				</View>
-				<View style={styles.btnContainer}>
-					<Ionicons
-						name="md-reload-circle"
-						title="Restart"
-						onPress={() => {
-							setKey((prevKey) => prevKey + 1);
-							setIsPaused(true);
-							setIsStart(true);
-						}}
-						size={80}
-						color={"#111010"}
-					/>
-				</View>
-				
 			</View>
-		</View>
 		</>
 	);
 }
